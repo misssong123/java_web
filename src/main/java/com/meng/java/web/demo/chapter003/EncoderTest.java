c520d9e554b31e743c969828356ec70807cf9315
孟松 <mengsong@58.com>
2023-10-18 09:56:14 +0800

    简单编码使用

--- src/main/java/com/meng/java/web/demo/chapter003/EncoderTest.java
+++ src/main/java/com/meng/java/web/demo/chapter003/EncoderTest.java
@@ -12,12 +12,12 @@ public class EncoderTest {
     public static void main(String[] args) throws Exception {
         String fileName = "1.txt";
         String charset = "UTF-8";
-        /*FileOutputStream fos = new FileOutputStream(fileName);
+        FileOutputStream fos = new FileOutputStream(fileName);
         OutputStreamWriter writer = new OutputStreamWriter(fos,charset);
         writer.write("测试字符写入");
         writer.flush();
         fos.close();
-        writer.close();*/
+        writer.close();
         FileInputStream fis = new FileInputStream(fileName);
         InputStreamReader reader = new InputStreamReader(fis,charset);
         char[] bytes = new char[1024];
